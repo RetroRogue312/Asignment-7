271b23d7d212f21eebe9a94500c75fefdcf5176d
Robert Lewis <caysonlewis@gmail.com>
2026-04-28 16:50:05 -0500

    Simplified to just using a matrix

--- src/Graph.java
+++ src/Graph.java
@@ -1,24 +1,69 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Arrays;
+import javax.imageio.ImageIO;
+
 public class Graph
 {
-    public Vector[] buildGraph(String sequence)
+    public void buildGraph(String sequence)
     {
         String[] seq = sequence.split(" ");
-        Vector[] nodes = new Vector[seq.length];
-        Vector Start = new Vector(seq[0]);
-        for (int i = 0;i < seq.length; i++)
+
+
+        int[][] matrix = new int[seq.length][seq.length];
+        for (int i = 0; i < matrix.length; i++)
         {
-            nodes[i] = new Vector(seq[i]);
+            Arrays.fill(matrix[i], 0);
         }
 
-        for(int i = 0; i < nodes.length; i++)
+        for(int i = 0; i < seq.length; i++)
         {
-            int out1 = (2 * i + 1) % nodes.length;
-            int out2 = (2 * i + 2) % nodes.length;
+            int out1 = (2 * i + 1) % seq.length;
+            int out2 = (2 * i + 2) % seq.length;
 
-            nodes[i].Out1 = nodes[out1];
-            nodes[i].Out2 = nodes[out2];
-
+            matrix[i][out1] = 1;
+            matrix[i][out2] = 1;
         }
-        return nodes;
+
+        drawGraph(matrix, seq);
     }
+
+    public void drawGraph(int[][] matrix, String[] strings)
+    {
+        String horizontal = "";
+        int maxLength = 0;
+        for (int i = 0; i < strings.length; i++)
+        {
+            if (maxLength < strings[i].length())
+                maxLength = strings[i].length();
+        }
+
+        String format = "%-" + (maxLength + 1) + "s";
+        String numFormat = "%-" + (maxLength + 1) + "d";
+
+        System.out.print(String.format(format, " "));
+        for (String s : strings)
+            System.out.print(String.format(format, s));
+        System.out.println();
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(String.format(format, strings[i])); // Row label
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(String.format(numFormat, matrix[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Graph g = new Graph();
+        String sequence = "ANT CUN BOG AMA DC TOL SAN";
+        g.buildGraph(sequence);
+    }
+
+
+
+
 }
